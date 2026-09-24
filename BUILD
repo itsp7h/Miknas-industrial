0729ff52379addffbbf04e9fa8b3ffba8deca9ce
SteelERP build <build@steelerp.invalid>
2026-09-24 15:48:16 +0000

    Build of bb3f2d9d7a607085405d0526678d4fd0e0981d70

---
+++ BUILD
@@ -0,0 +1,5 @@
+source   bb3f2d9d7a607085405d0526678d4fd0e0981d70
+built    2026-09-24T15:48:14Z
+php      8.2.34
+node     v20.20.2
+by       https://github.com/itsp7h/Miknas-industrial/actions/runs/36022559466
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,75 @@
+<?php return array (
+  'barryvdh/laravel-dompdf' => 
+  array (
+    'aliases' => 
+    array (
+      'PDF' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+      'Pdf' => 'Barryvdh\\DomPDF\\Facade\\Pdf',
+    ),
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\DomPDF\\ServiceProvider',
+    ),
+  ),
+  'laravel/reverb' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Reverb\\ApplicationManagerServiceProvider',
+      1 => 'Laravel\\Reverb\\ReverbServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'promoseven/azure-mailer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PromoSeven\\AzureMailer\\AzureMailerServiceProvider',
+    ),
+  ),
+  'promoseven/ultra-message' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PromoSeven\\UltraMessage\\UltraMessageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'UltraMessage' => 'PromoSeven\\UltraMessage\\Facades\\UltraMessage',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+);
